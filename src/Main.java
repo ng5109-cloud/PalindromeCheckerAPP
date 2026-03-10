9c7852f5defe345d026156b628493bac9d0608fb
ng5109-cloud <ng5109@srmist.edu.in>
2026-03-10 22:35:09 +0530

    UC3: description of the use case

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+        // Create a Scanner object to read input from the console
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("--- Palindrome Checker (UC3) ---");
+        System.out.print("Enter a string to check: ");
+        String original = scanner.nextLine();
+
+        // 1. Reverse string using a loop
+        String reversed = "";
+        
+        // Loop backwards from the end of the string to the start
+        for (int i = original.length() - 1; i >= 0; i--) {
+            // String Concatenation: Building the reversed string
+            // Note: This creates a new String object in every iteration
+            reversed += original.charAt(i);
+        }
+
+        // 2. Compare original and reversed using equals()
+        // We use .equals() because == checks memory reference, 
+        // while .equals() checks the actual sequence of characters.
+        if (original.equals(reversed)) {
+            System.out.println("Result: '" + original + "' is a palindrome.");
+        } else {
+            System.out.println("Result: '" + original + "' is NOT a palindrome.");
+        }
+
+        scanner.close();
     }
 }
